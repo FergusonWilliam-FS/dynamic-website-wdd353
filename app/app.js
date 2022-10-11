cd5eaadfa8e08baa75148f3513f94e841f41a4e9
WAFerguson1@student.fullsail.edu <WAFerguson1@student.fullsail.edu>
2022-10-11 09:56:18 -0700

    middle

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const router = require("../routes/routes");
 
 app.use(express.urlencoded({extended: true}));
 // all requests will handle json
@@ -13,5 +14,34 @@ app.use((req, res, next)=> {
         res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE');
         next();
     }
-    // middleware for EJS templating 18:55
 });
+    // middleware for EJS templating 18:55
+
+app.set('view-engine', 'ejs');
+app.engine('ejs', require('ejs').__express);
+
+
+// static site middleware
+app.use(express.static('public'));
+app.use(express.static('views'));
+app.use('/', router);
+
+
+// error handling middleware
+
+app.use((req, res, next) => {
+    const error = new Error('Not found!!!');
+    error.status = 404;
+    next(error)
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message,
+            status: error.status,
+        },
+    });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require("http");
 require("dotenv").config();
+const app = require("./app/app");
 
 http.createServer().listen(process.env.port, ()=> {
     console.log(`I am fully operational and all circuits are listening on ${process.env.port}`);
