9b978a76f839e8e5f91c3a0fda271945b27a79ef
WAFerguson1@student.fullsail.edu <WAFerguson1@student.fullsail.edu>
2022-10-11 11:37:43 -0700

    middle

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
     res.render('index', {
         pagename: 'Home',
     });
 });
+
+
+router.get('/about', (req, res) => {
+    res.render('about', {
+        pagename: 'About',
+    });
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact', {
+        pagename: 'Contact',
+    });
+});
+
+router.get('/services', (req, res) => {
+    res.render('services', {
+        pagename: 'Services',
+    });
+});
+
 
 module.exports = router;
